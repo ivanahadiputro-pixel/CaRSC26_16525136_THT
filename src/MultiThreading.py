ddac894186d8aa8ffe5e254463ef26a63911a25e
Iban <ivan.a.hadiputro@gmail.com>
2026-01-20 18:07:33 +0700

    Multithreading to src

---
+++ src/MultiThreading.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+def Video_Capture():
+    a = 0
+    while a == 0:
+        time.sleep(2)
+        print("Recieving Video Frame...")
+        
+
+def Telemetry():
+    with open("telemetry.txt", "r") as file:
+        for line in file:
+            time.sleep(3)
+            print(line.rstrip())
+            
+
+
+thread1 = threading.Thread(target=Video_Capture)
+thread1.start()
+
+thread2 = threading.Thread(target=Telemetry)
+thread2.start()
