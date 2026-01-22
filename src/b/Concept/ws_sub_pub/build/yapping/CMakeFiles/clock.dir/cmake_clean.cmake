466ba2a9831e272f7d249a61217acaa83df02103
ivanahadiputro-pixel <ivan.a.hadiputro@gmail.com>
2026-01-22 22:40:27 +0700

    Adding build folder to ws

---
+++ src/b/Concept/ws_sub_pub/build/yapping/CMakeFiles/clock.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clock.dir/src/clock.cpp.o"
+  "CMakeFiles/clock.dir/src/clock.cpp.o.d"
+  "clock"
+  "clock.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/b/Concept/ws_sub_pub/build/yapping/CMakeFiles/watch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/watch.dir/src/watch.cpp.o"
+  "CMakeFiles/watch.dir/src/watch.cpp.o.d"
+  "watch"
+  "watch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/watch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
